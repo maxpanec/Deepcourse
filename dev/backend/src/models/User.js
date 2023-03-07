266835ed2abc896d4e0dfdb65e25e38bcdfd8281
Max Panec <maxwellpanec@gmail.com>
2023-03-07 13:40:11 -0800

    update user db model

--- dev/backend/src/models/User.js
+++ dev/backend/src/models/User.js
@@ -4,8 +4,7 @@ const Joi = require('joi');
 const passwordComplexity = require("joi-password-complexity")
 
 const UserSchema = new mongoose.Schema({
-	firstName: {type: String, required: true},
-	lastName: {type: String, required: true},
+	username: {type: String, required: true, unique: true},
 	email: {type: String, required: true, unique: true},
 	password: {type: String, required: true},
     flashcardIDs: [{
@@ -28,8 +27,7 @@ const User = mongoose.model("users", UserSchema);
 
 const validate = (data) => {
 	const schema = Joi.object({
-		firstName: Joi.string().required().label("First Name"),
-		lastName: Joi.string().required().label("Last Name"),
+		username: Joi.string().required().label("Username"),
 		email: Joi.string().email().required().label("Email"),
 		password: passwordComplexity().required().label("Password")
 	})
