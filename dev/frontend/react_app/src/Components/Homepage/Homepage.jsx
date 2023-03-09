7f26d7df0f57cbf3b42917c21dd0ab25658f1add
Minh Hung <minhhung.nguyen@sjsu.edu>
2023-03-08 19:10:19 -0800

    Update Homepage.jsx
    cut the description short

--- dev/frontend/react_app/src/Components/Homepage/Homepage.jsx
+++ dev/frontend/react_app/src/Components/Homepage/Homepage.jsx
@@ -7,9 +7,8 @@ const HomePage = () => {
         <img src="https://i.imgur.com/TUtegR6.png" alt="Person studying" style={{ maxWidth: '100%', marginBottom: '20px' }} />
         <h1>Welcome to Study Sets</h1>
         <div style={{ maxWidth: '600px', margin: '0 auto', lineHeight: '1.5', fontSize: '18px' }}>
-          <p>This is a web app that helps users study for anything ranging from school exams to different kinds of cutlery. Users will be able to create and customize their own flashcards sets to help them learn their desired material. Users will also be able to take a quiz based on a given flashcard set; they will be able to select from a few different quiz formats and a quiz will be generated for them based on the original flashcards. Users can see a timeline of their quiz scores for a given flashcard set.</p>
+          <p>Study Sets allows users to create and customize their own flashcards sets to study any topic. You can take customizable quizzes that are generated based on the questions and answers from your flashcard set. It also offers quizzes with different formats and tracks users' performance in a timeline. Just create an account to get started.</p>
         </div>
-        <p>Here are some links to get you started:</p>
         <div>
           <a href="/about" style={{ display: 'inline-block', marginRight: '20px' }}>About Us</a>
           <a href="/contact" style={{ display: 'inline-block' }}>Contact Us</a>
