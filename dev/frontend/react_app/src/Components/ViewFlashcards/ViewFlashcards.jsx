1e97a98e1ccf8677871f24ac53c03025de2d963c
Minh Hung <minhhung.nguyen@sjsu.edu>
2023-04-07 12:43:25 -0700

    Update ViewFlashcards.jsx

--- dev/frontend/react_app/src/Components/ViewFlashcards/ViewFlashcards.jsx
+++ dev/frontend/react_app/src/Components/ViewFlashcards/ViewFlashcards.jsx
@@ -6,7 +6,9 @@ const ViewFlashcards = () => {
   const [quizzes, setQuizzes] = useState([
     { id: 1, title: 'Quiz 1', description: 'This is quiz 1', category: 'Category 1' },
     { id: 2, title: 'Quiz 2', description: 'This is quiz 2', category: 'Category 2' },
-    { id: 3, title: 'Quiz 3', description: 'This is quiz 3', category: 'Category 1' },
+    { id: 3, title: 'Quiz 3', description: 'This is quiz 3', category: 'Category 3' },
+    { id: 3, title: 'Quiz 4', description: 'This is quiz 4', category: 'Category 4' },
+    { id: 3, title: 'Quiz 5', description: 'This is quiz 5', category: 'Category 5' },
     // Add more quizzes as needed
   ]);
 
