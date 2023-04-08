5e9cdae13bf7916c3ea5d665ec8f3ad2d8a1ed5d
Justin Barragan <justinbarragan1@gmail.com>
2023-04-07 18:58:19 -0700

    Add dynamic rendering of individual sets

--- dev/frontend/react_app/src/Components/FlashcardView/FlashcardView.jsx
+++ dev/frontend/react_app/src/Components/FlashcardView/FlashcardView.jsx
@@ -1,80 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios"
 import "./FlashcardView.css";
 
-//sample flashcard set for testing purposes
-//currently loads data from the test object, not from the server
-let sampleFlashcardSet = {
-    //object holding the data for the study set
-    "data": {
-        //name of the study set
-        "name": "Sample Study Set",
-        //who made the study set
-        "createdBy": "Test Set Creator",
-        //array of objects with each object having keys question and answer representing 
-        //the question and answer for an individual flashcard
-        "cards": [
-            {
-                "question": "What is the name of our class?",
-                "answer": "CS-161: Software Project"
-            },
-            {
-                "question": "What color is the sky?",
-                "answer": "Blue"
-            },
-            {
-                "question": "Which university do we attend? And are you sure we attend there?",
-                "answer": "San Jose State University. Yes, I am positive. you can ask any of my buddies."
-            },
-            {
-                "question": "What year is it?",
-                "answer": "2023"
-            },
-            {
-                "question": "A deductive argument that has all true premises is ______",
-                "answer": "Sound"
-            },
-            {
-                "question": "How hot is the surface of the sun?",
-                "answer": "10,340 degrees Fahrenheit"
-            }
-        ],
-        "scores": []
-    }
-}
-
-
-let flashcards = sampleFlashcardSet["data"]["cards"];
-
-let cardData = flashcards.map((card) => 
-    <div className="complete-card">
-        <h2 class="question">{card["question"]}</h2>
-        <h2 class="answer">{card["answer"]}</h2>
-        </div>
-);
 const FlashcardView = (props) => {
-    //gets id from url parameter
-    const {id} = useParams();
-    let res;
+  const { id } = useParams();
+  const [cardData, setCardData] = useState([]);
+  const [titleData, setTitleData] = useState("");
+  let res;
+
+  useEffect(() => {
     const getRes = async () => {
         res = await axios.get("http://localhost:3001/flashcards/flashcard-set", {params: {id: id}});
-        console.log("res: " + res)
-    }
-    let result = getRes();
-    return(
-        <div className="outer">
-            <div className="title">
-                <h1 class="set-title">{sampleFlashcardSet["data"]["name"]}</h1>
-                <h2 className="creator">Created by: {sampleFlashcardSet["data"]["createdBy"]}</h2>
-                <h2>ID: {id}</h2> {/* can be removed, only here for testing purposes */}
-            </div>
-            <div className="flashcard-grid">
-                {cardData}
-            </div>
-        </div>
+        console.log("name: " + res.data.data.name); 
+        let flashcards = res.data.data.cards;
+        let title = (<div className="view-set-title">
+            <h1 className="set-title">{res.data.data.name}</h1>
+            <h2 className="creator">Created by: {res.data.data.createdBy}</h2>
+        </div>)
+        setTitleData(title);
+        let cards = flashcards.map((card) => 
+          <div className="complete-card">
+            <h2 class="question">{card["question"]}</h2>
+            <h2 class="answer">{card["answer"]}</h2>
+          </div>
+        );
+        setCardData(cards);
+      }
+    getRes();
+  }, [id]);
 
-    );
+  return(
+    <div className="outer">
+        {titleData}
+      <div className="flashcard-grid">
+        {cardData}
+      </div>
+    </div>
+  );
 };
 
-export default FlashcardView
+export default FlashcardView;
