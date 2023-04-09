0c592d937c52e35fb9ab646b9f57230982abd674
Minh Hung <minhhung.nguyen@sjsu.edu>
2023-04-09 14:58:44 -0700

    replace id with the actual id

--- dev/frontend/react_app/src/Components/ViewStudySets/ViewStudySets.jsx
+++ dev/frontend/react_app/src/Components/ViewStudySets/ViewStudySets.jsx
@@ -1,40 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './ViewStudySets.css';
 
 const ViewStudySets = (props) => {
   const navigate = useNavigate();
 
-  const [studySets, setStudySets] = useState([
-    { id: 1, name: 'Study Set 1', category: 'Science', description: 'This is Study Set 1' },
-    { id: 2, name: 'Study Set 2', category: 'History', description: 'This is Study Set 2' },
-    { id: 3, name: 'Study Set 3', category: 'Math', description: 'This is Study Set 3' },
-    // Add more sample data as needed
-  ]);
+  const [studySets, setStudySets] = useState([]);
 
   useEffect(() => {
     const fetchStudySets = async () => {
       try {
-        const response = await axios.get('http://localhost:3001/flashcards/flashcard-sets-info', 
-        {params: {username: props.user.username}}
-        );
-        setStudySets(response.data.data)
-        // navigate("/flashcard-view/64153e33e22b57cda9b80ffa")
-      }
-      catch (error) {
-        console.log("test error")
+        const response = await axios.get('http://localhost:3001/flashcards/flashcard-sets-info', {
+          params: { username: props.user.username },
+        });
+        setStudySets(response.data.data);
+      } catch (error) {
+        console.log('Failed to fetch study sets:', error);
       }
     };
-    fetchStudySets()
+    fetchStudySets();
   }, [props.user.username]);
 
-  console.log(studySets)
-
   const handleStudySetClick = (setId) => {
-    // Handle the study set click event, e.g. redirect to study set page
+    // Handle the study set click event, e.g. navigate to study set page
     console.log('Study Set clicked with id:', setId);
+    navigate(`/flashcard-view/${setId}`); // Use navigate function to navigate to the desired route
   };
+
   const handleRemoveStudySet = (setId) => {
     // Handle the study set removal event, e.g. send a request to remove study set
     console.log('Study Set removed with id:', setId);
@@ -45,7 +38,7 @@ const ViewStudySets = (props) => {
       <h1 className="study-sets-title">Study Sets</h1>
       <div className="study-sets-grid">
         {studySets.map((set) => (
-          <div key={set.id} className="study-set-box">
+          <div key={set.id} className="study-set-box" onClick={() => handleStudySetClick(set.id)}>
             <h3 className="study-set-name">{set.name}</h3>
             <p className="study-set-category">Category: {set.category}</p>
             <p className="study-set-description">Description: {set.description}</p>
