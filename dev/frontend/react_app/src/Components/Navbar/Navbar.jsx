ff8bda03e8daf01d0dc051ab49ca9c0125e372a1
Minh Hung <minhhung.nguyen@sjsu.edu>
2023-04-09 19:44:33 -0700

    Update Navbar.jsx

--- dev/frontend/react_app/src/Components/Navbar/Navbar.jsx
+++ dev/frontend/react_app/src/Components/Navbar/Navbar.jsx
@@ -24,11 +24,6 @@ const Navbar = (props)  => {
             <div className='navbar_links'>
                 <ul>
                     <Link to='/'><li className='navbar_item'>Home</li></Link>
-                    {/* Eventually, this link should be removed from the Navbar */}
-                    {/* Should only be accessed by clicking on an individual study set from the view-all page */}
-                    {/* The parameter after flashcard-view/ is the ID of the individual flashcard set */}
-                    <Link to='/flashcard-view/6430c007f2eced61991f2b1d'><li className='navbar_item'>View Flashcard Set</li></Link>
-
                     <Link to='/create-set' onClick={() => window.location.href('/create-set')}><li className='navbar_item'>Create Flashcards</li></Link> 
                     <Link to='/view-studysets'><li className='navbar_item'>View Flashcards</li></Link> 
 
