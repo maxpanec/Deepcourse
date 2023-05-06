336a3c3b5976cab270041cf2041f0991f7bb1f67
Minh Hung <minhhung.nguyen@sjsu.edu>
2023-05-05 18:03:48 -0700

    Comments for FlashcardView

--- dev/frontend/react_app/src/Components/QuizScores/QuizScores.jsx
+++ dev/frontend/react_app/src/Components/QuizScores/QuizScores.jsx
@@ -8,11 +8,16 @@ const QuizScores = (props) => {
     const [scores, setScores] = useState([]);
     const [startIndex, setStartIndex] = useState(0);
 
+    /**
+     * Fetches quiz scores for a specific user and sorts them by date.
+     *
+     * @param {string} id The user's ID to fetch scores for
+     */
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('http://localhost:3001/quiz/scores', {
-                    params: { id: id }
+                    params: { id: id },
                 });
                 const sortedScores = response.data.data.sort((a, b) => new Date(b.date) - new Date(a.date));
                 setScores(sortedScores);
@@ -23,36 +28,60 @@ const QuizScores = (props) => {
         fetchData();
     }, [id]);
 
+    /**
+     * Calculates the height of a bar in a chart based on a score.
+     *
+     * @param {number} score The score to calculate the bar height for
+     * @return {string} The height of the bar in pixels
+     */
     const getBarHeight = (score) => {
         if (score === 0) {
             return '100px';
         } else {
-            const barHeight = score / 100; // adjust this calculation as needed
-            return `${barHeight * 400}px`; //
+            const barHeight = score / 100;
+            return `${barHeight * 400}px`;
         }
     };
 
+    /**
+     * Formats a date string into a readable date and time format.
+     *
+     * @param {string} dateString The date string to format
+     * @return {string} The formatted date and time string
+     */
     const formatDate = (dateString) => {
         const date = new Date(dateString);
-        const formattedDate = `${date.toLocaleDateString()}`
-        const formattedTime = `${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`
+        const formattedDate = `${date.toLocaleDateString()}`;
+        const formattedTime = `${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
         return `${formattedDate}\n${formattedTime}`;
     };
 
+    /**
+     * Handles the "next" button click by updating the start index if there are more scores to display.
+     */
     const handleNextClick = () => {
         if (startIndex + 5 < scores.length) {
             setStartIndex(startIndex + 5);
         }
     };
 
+    /**
+     * Handles the "previous" button click by updating the start index if there are more scores to display.
+     */
     const handlePrevClick = () => {
         if (startIndex - 5 >= 0) {
             setStartIndex(startIndex - 5);
         }
     };
 
+    /**
+     * Extracts a slice of scores to display based on the current start index.
+     */
     const displayedScores = scores.slice(startIndex, startIndex + 5);
 
+
+
+    // Render the component with the scores data
     return (
         <div className="quiz-scores-container">
             <h2 className="quiz-scores-heading">Quiz Scores</h2>
@@ -78,6 +107,6 @@ const QuizScores = (props) => {
             </div>
         </div>
     );
-}
+};
 
 export default QuizScores;
